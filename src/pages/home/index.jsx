d05b02398c4a4cd1276c00c4f5f9754de3458ffb
Carolaine <karol-21021998@hotmail.com>
2024-09-05 16:34:03 -0300

    feat: add iamge and styles

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,17 @@
-import './styles.css'
+import "./styles.css";
 
 export default function Home() {
-    return (
-        <div className='container'>
-            <h1>Seja bem vindo à MeowFacts!</h1>
-            <p>Saiba tudo sobre o mundo felino e leve a vida mais rom-rom! Aqui você verá dicas, curiosidades sobre gatos.</p>          
-        </div>
-    );
+  return (
+    <div className="container">
+      <div className="NavBar">Aqui o nome ou a Logo do site</div>
+      <h1>
+        Bem-vindos à <br /> Sr<strong className="strongHome">.</strong> Bigodes!
+      </h1>
+
+      <p>
+        Saiba tudo sobre o mundo felino e leve a vida mais rom-rom!
+        <br /> Aqui você verá dicas, curiosidades sobre gatos.
+      </p>
+    </div>
+  );
 }
