4c394a7ca54d81a45ff606076020193c6336d8e9
DayaneNogueira <dayane.nogueira@alunos.sc.senac.br>
2024-09-09 22:23:06 -0300

    feat: add new styles

--- src/pages/Cadastro/index.jsx
+++ src/pages/Cadastro/index.jsx
@@ -1,7 +1,14 @@
 import CardCadastro from "../../components/Card/CardCadastro";
+import './styles.css'
 
 export default function Cadastro() {
     return (
+        <>
+        <div className="div-cadastro">
+            <h2 className="title-cadastro">Faça seu cadastro!</h2>
+        </div>
+        
         <CardCadastro />
+        </>
 );
 }
